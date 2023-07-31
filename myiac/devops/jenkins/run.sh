3851a0a577ae7946c8f68d969e3190039ce6de2b
yzj-new <43331052+yzj-new@users.noreply.github.com>
2023-08-01 00:47:27 +0800

    Create run.sh

---
+++ myiac/devops/jenkins/run.sh
@@ -0,0 +1,6 @@
+## LoadDockerImage
+kind load  docker-image jenkins/jenkins:2.415-jdk11 --name devopscluster
+kind load  docker-image jenkins/inbound-agent:latest --name devopscluster
+
+## ArgoAPP
+kubectl -n argocd apply -f jenkins-argoapp.yaml
